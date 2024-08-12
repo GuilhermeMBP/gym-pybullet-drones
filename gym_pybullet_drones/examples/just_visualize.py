643911301292d824dd5542258ec550a002f1ea1f
Guilherme Pereira <2211062@my.ipleiria.pt>
2024-08-12 15:24:27 +0000

    changed to support 25 actions

--- gym_pybullet_drones/examples/just_visualize.py
+++ gym_pybullet_drones/examples/just_visualize.py
@@ -39,30 +39,17 @@ DEFAULT_OUTPUT_FOLDER = 'results'
 DEFAULT_COLAB = False
 
 DEFAULT_OBS = ObservationType('kin') # 'kin' or 'rgb'
-DEFAULT_ACT = ActionType('discrete_2d') # 'rpm' or 'pid' or 'vel' or 'one_d_rpm' or 'one_d_pid'
+DEFAULT_ACT = ActionType('discrete_2d_complex') # 'rpm' or 'pid' or 'vel' or 'one_d_rpm' or 'one_d_pid'
 DEFAULT_AGENTS = 2
 DEFAULT_MA = False
 DEFAULT_STEP = 0.1
 DEFAULT_AXIS = 'y'
 
 def run(multiagent=DEFAULT_MA, output_folder=DEFAULT_OUTPUT_FOLDER, gui=DEFAULT_GUI, plot=True, colab=DEFAULT_COLAB, record_video=DEFAULT_RECORD_VIDEO, local=True, step=DEFAULT_STEP, x=0, y=0, z=0.1125, step_axis=DEFAULT_AXIS):
-    DEFAULT_OBS = ObservationType('kin') # 'kin' or 'rgb'
 
-    filename = os.path.join(output_folder, '/app/results/save-07.15.2024_kin_discrete_2d')
+    filename = os.path.join(output_folder, '/app/results/save-08.02.2024_15.16.31')
     if not os.path.exists(filename):
         os.makedirs(filename+'/')
-
-    if not multiagent:
-        train_env = make_vec_env(HoverAviary,
-                                 env_kwargs=dict(obs=DEFAULT_OBS, act=DEFAULT_ACT),
-                                 n_envs=1,
-                                 seed=0
-                                 )
-        eval_env = HoverAviary(obs=DEFAULT_OBS, act=DEFAULT_ACT)
-
-    #### Check the environment's spaces ########################
-    print('[INFO] Action space:', train_env.action_space)
-    print('[INFO] Observation space:', train_env.observation_space)
 
     #### Print training progression ############################
     best = 0
@@ -88,6 +75,10 @@ def run(multiagent=DEFAULT_MA, output_folder=DEFAULT_OUTPUT_FOLDER, gui=DEFAULT_
         print("[ERROR]: no model un5der the specified path", filename)
     model = SAC.load(path)
 
+    if z < 0.1125:
+        print("Z coordinate is less than 0.1125, setting it to 0.1125")
+        z = 0.1125
+    
     #### Show (and record a video of) the model's performance ##
     if not multiagent:
         test_env = HoverAviary(gui=gui,
@@ -116,13 +107,13 @@ def run(multiagent=DEFAULT_MA, output_folder=DEFAULT_OUTPUT_FOLDER, gui=DEFAULT_
     elif step_axis == 'z':
         index_axis = 2
     else:
-        raise ValueError("Invalid step_axis")
+        print("Invalid step_axis, using y as default")
 
     obs, info = test_env.reset(seed=42, options={})
     start = time.time()
     #variable to be used in the register of the dataset
     j = 1
-    dir_name = 'dataset'+datetime.now().strftime("%m.%d.%Y_%H.%M.%S")
+    dir_name = 'dataset'+datetime.now().strftime("%m.%d.%Y_%H.%M.%S")+'_start_'+str(x)+'_'+str(y)+'_'+str(z)
     for i in range((test_env.EPISODE_LEN_SEC)*test_env.CTRL_FREQ):
         action, _states = model.predict(obs,
                                         deterministic=True
@@ -131,16 +122,8 @@ def run(multiagent=DEFAULT_MA, output_folder=DEFAULT_OUTPUT_FOLDER, gui=DEFAULT_
         obs2 = obs.squeeze()
         act2 = action.squeeze()
         print("Obs:", obs, "\tAction", action, "\tReward:", reward, "\tTerminated:", terminated, "\tTruncated:", truncated)
-        
-        #this is to register the obs at the 5 second mark
-        if i == 5*test_env.CTRL_FREQ:
-            obs3 = obs2
 
-        #this is to register the obs at the point that it passes the y=0.5 mark
-        if obs2[1] > 0.5 and "obs4" not in locals():
-            obs4 = obs2
-
-        if DEFAULT_ACT != ActionType.DISCRETE_2D and DEFAULT_ACT != ActionType.DISCRETE_3D and DEFAULT_OBS == ObservationType.KIN:
+        if DEFAULT_ACT != ActionType.DISCRETE_2D and DEFAULT_ACT != ActionType.DISCRETE_3D and DEFAULT_ACT != ActionType.DISCRETE_2D_COMPLEX and DEFAULT_OBS == ObservationType.KIN:
             if not multiagent:
                 logger.log(drone=0,
                     timestamp=i/test_env.CTRL_FREQ,
@@ -166,7 +149,7 @@ def run(multiagent=DEFAULT_MA, output_folder=DEFAULT_OUTPUT_FOLDER, gui=DEFAULT_
                         control=np.zeros(12)
                         )
         elif DEFAULT_OBS == ObservationType.POS_RPY or DEFAULT_OBS == ObservationType.KIN:
-            if DEFAULT_ACT == ActionType.DISCRETE_2D or DEFAULT_ACT == ActionType.DISCRETE_3D:
+            if DEFAULT_ACT == ActionType.DISCRETE_2D or DEFAULT_ACT == ActionType.DISCRETE_3D or DEFAULT_ACT == ActionType.DISCRETE_2D_COMPLEX:
                 action_to_take = np.argmax(act2)
                 rpm = test_env._getRPMs(action_to_take)
                 logger.log(drone=0,
@@ -176,17 +159,17 @@ def run(multiagent=DEFAULT_MA, output_folder=DEFAULT_OUTPUT_FOLDER, gui=DEFAULT_
                                         obs2[3:6],
                                         np.zeros(5),
                                         reward,
-                                        rpm[:]
+                                        rpm[0,:]
                                         ]),
                     control=np.zeros(12)
                     )
-                #if step is positive and the coordinate in [step_axis] is greater than (starting coordinate in step_axis) + step, (if step = default (0.1) and starting coordinate in step_axis = 0 then 0.1, 0.2 0.3 etc) 
+                #if step is positive and the coordinate in [step_axis] is greater than (starting coordinate in step_axis) + step, (if step = default (0.1) and starting coordinate in [step_axis] = 0 then 0.1, 0.2 0.3 etc) 
                 # or if step is negative and ... is less than ... then
                 # register the obs and action
                 if (step > 0 and obs2[index_axis] > (j * step) + test_env.INIT_XYZS[0][index_axis]) or (step < 0 and obs2[index_axis] < (j * step) + test_env.INIT_XYZS[0][index_axis]):
                     if not os.path.exists(dir_name):
                         os.makedirs(dir_name)
-                    with open(dir_name+'/obs_act_y_equals_'+ str(j).zfill(3)+'.csv', 'w') as file:
+                    with open(dir_name+'/obs_act_'+step_axis+'_equals_'+ str(j).zfill(3)+'.csv', 'w') as file:
                         for obs_mom in obs2:
                             file.write(f'{obs_mom}\n')
                         file.write(f'{action_to_take}\n')
@@ -198,8 +181,6 @@ def run(multiagent=DEFAULT_MA, output_folder=DEFAULT_OUTPUT_FOLDER, gui=DEFAULT_
         if terminated:
             obs = test_env.reset(seed=42, options={})
     test_env.close()
-
-    print(obs4)
 
     #if plot and DEFAULT_OBS == ObservationType.KIN:
     logger.plot()
