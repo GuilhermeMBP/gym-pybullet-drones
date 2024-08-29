158b4b87f3cef399c994eef257c0b40cfbecd54e
Guilherme Pereira <2211062@my.ipleiria.pt>
2024-08-29 12:16:58 +0000

    script to visualize the simulation with some model

---
+++ gym_pybullet_drones/examples/just_vis.py
@@ -0,0 +1,223 @@
+"""Script demonstrating the use of `gym_pybullet_drones`'s Gymnasium interface.
+
+Classes HoverAviary and MultiHoverAviary are used as learning envs for the PPO algorithm.
+
+Example
+-------
+In a terminal, run as:
+
+    $ python learn.py --multiagent false
+    $ python learn.py --multiagent true
+
+Notes
+-----
+This is a minimal working example integrating `gym-pybullet-drones` with 
+reinforcement learning library `stable-baselines3`.
+
+"""
+import os
+import time
+from datetime import datetime
+import argparse
+import gymnasium as gym
+import numpy as np
+import torch
+from stable_baselines3 import SAC
+from stable_baselines3.common.env_util import make_vec_env
+from stable_baselines3.common.callbacks import EvalCallback, StopTrainingOnRewardThreshold
+from stable_baselines3.common.evaluation import evaluate_policy
+
+from gym_pybullet_drones.utils.Logger import Logger
+from gym_pybullet_drones.envs.HoverAviary import HoverAviary
+from gym_pybullet_drones.envs.MultiHoverAviary import MultiHoverAviary
+from gym_pybullet_drones.utils.utils import sync, str2bool
+from gym_pybullet_drones.utils.enums import ObservationType, ActionType
+
+DEFAULT_GUI = True
+DEFAULT_RECORD_VIDEO = False
+DEFAULT_OUTPUT_FOLDER = 'results'
+DEFAULT_COLAB = False
+
+DEFAULT_OBS = ObservationType('kin') # 'kin' or 'rgb'
+DEFAULT_ACT = ActionType('discrete_2d_complex') # 'rpm' or 'pid' or 'vel' or 'one_d_rpm' or 'one_d_pid'
+DEFAULT_AGENTS = 2
+DEFAULT_MA = False
+
+def run(multiagent=DEFAULT_MA, output_folder=DEFAULT_OUTPUT_FOLDER, gui=DEFAULT_GUI, plot=True, colab=DEFAULT_COLAB, record_video=DEFAULT_RECORD_VIDEO, local=True):
+
+    filename = os.path.join(output_folder, '/app/results/save-08.14.2024_06.39.34')
+    if not os.path.exists(filename):
+        os.makedirs(filename+'/')
+
+    #### Print training progression ############################
+    best = 0
+    with np.load(filename+'/evaluations.npz') as data:
+        for j in range(data['timesteps'].shape[0]):
+            print(str(data['timesteps'][j])+","+str(data['results'][j][0]))
+            if data['results'][j][0] > best:
+                best = data['results'][j][0]
+
+    print("Best reward:", best)
+
+    ############################################################
+    ############################################################
+    ############################################################
+    ############################################################
+    ############################################################
+
+    # if os.path.isfile(filename+'/final_model.zip'):
+    #     path = filename+'/final_model.zip'
+    if os.path.isfile(filename+'/best_model.zip'):
+        path = filename+'/best_model.zip'
+    else:
+        print("[ERROR]: no model under the specified path", filename)
+    model = SAC.load(path)
+
+    #### Show (and record a video of) the model's performance ##
+    if not multiagent:
+        test_env = HoverAviary(gui=gui,
+                               obs=DEFAULT_OBS,
+                               act=DEFAULT_ACT,
+                               record=record_video, 
+                               initial_xyzs=np.array([[0,0,0.1125]]))
+        test_env_nogui = HoverAviary(obs=DEFAULT_OBS, act=DEFAULT_ACT)
+    else:
+        test_env = MultiHoverAviary(gui=gui,
+                                        num_drones=DEFAULT_AGENTS,
+                                        obs=DEFAULT_OBS,
+                                        act=DEFAULT_ACT,
+                                        record=record_video)
+        test_env_nogui = MultiHoverAviary(num_drones=DEFAULT_AGENTS, obs=DEFAULT_OBS, act=DEFAULT_ACT)
+    logger = Logger(logging_freq_hz=int(test_env.CTRL_FREQ),
+                num_drones=DEFAULT_AGENTS if multiagent else 1,
+                output_folder=output_folder,
+                colab=colab
+                )
+
+    mean_reward, std_reward = evaluate_policy(model,
+                                              test_env_nogui,
+                                              n_eval_episodes=10
+                                              )
+    print("\n\n\nMean reward ", mean_reward, " +- ", std_reward, "\n\n")
+
+    obs, info = test_env.reset(seed=42, options={})
+    if DEFAULT_ACT == ActionType.DISCRETE_2D_COMPLEX:
+        counter = np.zeros(25)
+    elif DEFAULT_ACT == ActionType.DISCRETE_2D:
+        counter = np.zeros(7)
+
+    start = time.time()
+    for i in range((test_env.EPISODE_LEN_SEC)*test_env.CTRL_FREQ):
+        action, _states = model.predict(obs,
+                                        deterministic=True
+                                        )
+        obs, reward, terminated, truncated, info = test_env.step(action)
+        obs2 = obs.squeeze()
+        act2 = action.squeeze()
+        print("Obs:", obs, "\tAction", action, "\tReward:", reward, "\tTerminated:", terminated, "\tTruncated:", truncated) 
+        
+        if i == 5*test_env.CTRL_FREQ:
+            obs3 = obs2
+
+        if DEFAULT_OBS == ObservationType.KIN:
+            if not multiagent:
+                if DEFAULT_ACT == ActionType.DISCRETE_2D_COMPLEX:
+                    action_to_take = np.argmax(act2)
+                    counter[action_to_take] += 1
+                    rpm_actual = test_env._getRPMs(action_to_take)
+                    logger.log(drone=0,
+                        timestamp=i/test_env.CTRL_FREQ,
+                        state=np.hstack([obs2[0:3],
+                                            np.zeros(4),
+                                            obs2[3:15],
+                                            rpm_actual[0,0],
+                                            rpm_actual[0,1],
+                                            rpm_actual[0,2],
+                                            rpm_actual[0,3]
+                                            ]),
+                        control=np.zeros(12)
+                        )
+                else:
+                    logger.log(drone=0,
+                        timestamp=i/test_env.CTRL_FREQ,
+                        state=np.hstack([obs2[0:3],
+                                            np.zeros(4),
+                                            obs2[3:15],
+                                            act2[1],
+                                            act2[1],
+                                            act2[0],
+                                            act2[0]
+                                            ]),
+                        control=np.zeros(12)
+                        )
+            else:
+                for d in range(DEFAULT_AGENTS):
+                    logger.log(drone=d,
+                        timestamp=i/test_env.CTRL_FREQ,
+                        state=np.hstack([obs2[d][0:3],
+                                            np.zeros(4),
+                                            obs2[d][3:15],
+                                            act2[d]
+                                            ]),
+                        control=np.zeros(12)
+                        )
+        elif DEFAULT_OBS == ObservationType.POS_RPY:
+            action_to_take = np.argmax(act2)
+            strong = 0.005
+            weak = 0.0025
+            rpm = np.zeros(4)
+            counter[action_to_take] += 1
+            if action_to_take == 0: #STRONG LEFT
+                rpm[:2] = np.repeat(test_env.HOVER_RPM * (1+strong), 2)
+                rpm[2:] = np.repeat(test_env.HOVER_RPM, 2)
+            elif action_to_take == 1: #WEAK LEFT
+                rpm[:2] = np.repeat(test_env.HOVER_RPM * (1+weak), 2)
+                rpm[2:] = np.repeat(test_env.HOVER_RPM, 2)
+                print("\n\n\n\nweak was selected!!\n\n\n\n")
+            elif action_to_take == 2: #UP
+                rpm[:] = np.repeat(test_env.HOVER_RPM * (1+strong), 4)
+            elif action_to_take == 3: #HOVER
+                rpm[:] = np.repeat(test_env.HOVER_RPM, 4)
+            elif action_to_take == 4: #DOWN
+                rpm[:] = np.repeat(test_env.HOVER_RPM * (1-strong), 4)
+            elif action_to_take == 5: #WEAK RIGHT
+                rpm[:2] = np.repeat(test_env.HOVER_RPM, 2)
+                rpm[2:] = np.repeat(test_env.HOVER_RPM * (1+weak), 2)
+                print("\n\n\n\nweak was selected!!\n\n\n\n")
+            elif action_to_take == 6: #STRONG RIGHT
+                rpm[:2] = np.repeat(test_env.HOVER_RPM, 2)
+                rpm[2:] = np.repeat(test_env.HOVER_RPM * (1+strong), 2)
+            logger.log(drone=0,
+                    timestamp=i/test_env.CTRL_FREQ,
+                    state=np.hstack([obs2[0:3],
+                                        np.zeros(4),
+                                        obs2[3:6],
+                                        np.zeros(5),
+                                        reward,
+                                        rpm[:]
+                                        ]),
+                    control=np.zeros(12)
+                    )
+        test_env.render()
+        print(terminated)
+        sync(i, start, test_env.CTRL_TIMESTEP)
+        if terminated:
+            obs = test_env.reset(seed=42, options={})
+    test_env.close()
+
+    print(obs3)
+    print(counter)
+    #if plot and DEFAULT_OBS == ObservationType.KIN:
+    logger.plot()
+
+if __name__ == '__main__':
+    #### Define and parse (optional) arguments for the script ##
+    parser = argparse.ArgumentParser(description='Single agent reinforcement learning example script')
+    parser.add_argument('--multiagent',         default=DEFAULT_MA,            type=str2bool,      help='Whether to use example LeaderFollower instead of Hover (default: False)', metavar='')
+    parser.add_argument('--gui',                default=DEFAULT_GUI,           type=str2bool,      help='Whether to use PyBullet GUI (default: True)', metavar='')
+    parser.add_argument('--record_video',       default=DEFAULT_RECORD_VIDEO,  type=str2bool,      help='Whether to record a video (default: False)', metavar='')
+    parser.add_argument('--output_folder',      default=DEFAULT_OUTPUT_FOLDER, type=str,           help='Folder where to save logs (default: "results")', metavar='')
+    parser.add_argument('--colab',              default=DEFAULT_COLAB,         type=bool,          help='Whether example is being run by a notebook (default: "False")', metavar='')
+    ARGS = parser.parse_args()
+
+    run(**vars(ARGS))
