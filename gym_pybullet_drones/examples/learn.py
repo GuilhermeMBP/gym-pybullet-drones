1ab05a42869a0b01e631db8bfb7524dc6ec4b161
Guilherme Pereira <2211062@my.ipleiria.pt>
2024-09-05 15:02:54 +0000

    rename file

--- gym_pybullet_drones/examples/learn.py
+++ gym_pybullet_drones/examples/learn.py
@@ -22,7 +22,10 @@ import argparse
 import gymnasium as gym
 import numpy as np
 import torch
-from stable_baselines3 import PPO
+import wandb
+import optuna
+import gc
+from stable_baselines3 import SAC
 from stable_baselines3.common.env_util import make_vec_env
 from stable_baselines3.common.callbacks import EvalCallback, StopTrainingOnRewardThreshold
 from stable_baselines3.common.evaluation import evaluate_policy
@@ -39,12 +42,55 @@ DEFAULT_OUTPUT_FOLDER = 'results'
 DEFAULT_COLAB = False
 
 DEFAULT_OBS = ObservationType('kin') # 'kin' or 'rgb'
-DEFAULT_ACT = ActionType('two_d_rpm') # 'rpm' or 'pid' or 'vel' or 'one_d_rpm' or 'one_d_pid'
+DEFAULT_ACT = ActionType('discrete_2d_complex') # 'rpm' or 'pid' or 'vel' or 'one_d_rpm' or 'one_d_pid'
 DEFAULT_AGENTS = 2
 DEFAULT_MA = False
 
-def run(multiagent=DEFAULT_MA, output_folder=DEFAULT_OUTPUT_FOLDER, gui=DEFAULT_GUI, plot=True, colab=DEFAULT_COLAB, record_video=DEFAULT_RECORD_VIDEO, local=True):
+def run(multiagent=DEFAULT_MA, 
+        output_folder=DEFAULT_OUTPUT_FOLDER, 
+        gui=DEFAULT_GUI, 
+        plot=True, 
+        colab=DEFAULT_COLAB, 
+        record_video=DEFAULT_RECORD_VIDEO, 
+        local=True, 
+        #new simulator variables
+        learning_rate=0.0004, 
+        gamma=0.99, 
+        batch_size=128, 
+        net_arch={'pi': [64, 64], 'qf': [128, 128]},
+        ent_coef=0.01, 
+        target_entropy=-1.0,
+        num_epochs=250,
+        total_steps=100000,
+        num_envs=4
+        ):
 
+    # Initialize W&B
+    wandb.login()
+
+    run = wandb.init(
+        project="gym-pybullet-drones", 
+        entity="greis",
+        config={
+            "learning_rate": learning_rate,
+            "gamma": gamma,
+            "batch_size": batch_size,
+            "buffer_size": 500000,
+            "tau": 0.005,
+            "train_freq": 64,
+            "gradient_steps": 64,
+            "n_eval_episodes": 10,
+            "eval_freq": 5000,
+            "epochs": num_epochs,
+            "net_arch": net_arch,
+            "ent_coef": ent_coef,
+            "target_entropy": target_entropy
+        },
+    )
+
+    # Explicitly log the learning rate on Wandb
+    wandb.log({"learning_rate": learning_rate})
+    
     filename = os.path.join(output_folder, 'save-'+datetime.now().strftime("%m.%d.%Y_%H.%M.%S"))
     if not os.path.exists(filename):
         os.makedirs(filename+'/')
@@ -69,18 +115,26 @@ def run(multiagent=DEFAULT_MA, output_folder=DEFAULT_OUTPUT_FOLDER, gui=DEFAULT_
     print('[INFO] Observation space:', train_env.observation_space)
 
     #### Train the model #######################################
-    policy_kwargs = dict(activation_fn=torch.nn.ReLU,
-                     net_arch=dict(pi=[256, 256, 256], vf=[256, 256, 256]))
+    policy_kwargs = dict(
+                        activation_fn=torch.nn.ReLU,
+                        net_arch=net_arch
+                        ) 
     
-    model = PPO('MlpPolicy',
+    model = SAC('MlpPolicy',
                 train_env,
                 # tensorboard_log=filename+'/tb/',
                 policy_kwargs=policy_kwargs,
-                verbose=1)
+                device='cuda',
+                learning_rate=learning_rate,
+                gamma=gamma,
+                batch_size=128,
+                verbose=1,
+                ent_coef=ent_coef,
+                target_entropy=target_entropy)
 
     #### Target cumulative rewards (problem-dependent) ##########
-    if DEFAULT_ACT == ActionType.ONE_D_RPM or DEFAULT_ACT == ActionType.TWO_D_RPM:
-        target_reward = 460 if not multiagent else 949.5
+    if DEFAULT_ACT == ActionType.ONE_D_RPM:
+        target_reward = 474.15 if not multiagent else 949.5
     else:
         target_reward = 467. if not multiagent else 920.
     callback_on_best = StopTrainingOnRewardThreshold(reward_threshold=target_reward,
@@ -93,6 +147,14 @@ def run(multiagent=DEFAULT_MA, output_folder=DEFAULT_OUTPUT_FOLDER, gui=DEFAULT_
                                  eval_freq=int(1000),
                                  deterministic=True,
                                  render=False)
+    
+   # Use WandbCallback for logging to wandb
+
+    wandb_callback = WandbCallback(
+        gradient_save_freq=100,
+        model_save_path=filename+'/',
+        verbose=2)
+    
     model.learn(total_timesteps=int(1e7) if local else int(1e2), # shorter training in GitHub Actions pytest
                 callback=eval_callback,
                 log_interval=100)
@@ -121,7 +183,7 @@ def run(multiagent=DEFAULT_MA, output_folder=DEFAULT_OUTPUT_FOLDER, gui=DEFAULT_
         path = filename+'/best_model.zip'
     else:
         print("[ERROR]: no model under the specified path", filename)
-    model = PPO.load(path)
+    model = SAC.load(path)
 
     #### Show (and record a video of) the model's performance ##
     if not multiagent:
@@ -188,8 +250,44 @@ def run(multiagent=DEFAULT_MA, output_folder=DEFAULT_OUTPUT_FOLDER, gui=DEFAULT_
             obs = test_env.reset(seed=42, options={})
     test_env.close()
 
-    if plot and DEFAULT_OBS == ObservationType.KIN:
-        logger.plot()
+    del train_env, eval_env, test_env, model, test_env_nogui, logger
+    gc.collect()
+    #if plot and DEFAULT_OBS == ObservationType.KIN:
+    #    logger.plot()
+    return mean_reward
+
+#Add a objective funtion to optimize the hiperparameters on Optuna
+def objective(trial):
+    learning_rate = trial.suggest_float('learning_rate', 1e-5, 1e-1, log=True)
+    batch_size = trial.suggest_categorical('batch_size', [32, 64, 128, 512])
+    gamma = trial.suggest_float('gamma', 0.9, 0.9999)
+    # Redes separadas para pi e qf
+    pi_net_arch_str = trial.suggest_categorical('pi_net_arch', ['(32, 32)', '(64, 64)', '(128, 128)', '(256, 256)'])
+    qf_net_arch_str = trial.suggest_categorical('qf_net_arch', ['(32, 32)', '(64, 64)', '(128, 128)', '(256, 256)'])
+    pi_net_arch = tuple(map(int, pi_net_arch_str.strip('()').split(', ')))
+    qf_net_arch = tuple(map(int, qf_net_arch_str.strip('()').split(', ')))
+    ent_coef = trial.suggest_float('ent_coef', 1e-5, 0.1, log=True)
+    target_entropy = trial.suggest_float('target_entropy', -5.0, 0.0)
+
+    # Call the run function with these hyperparameters
+    mean_reward = run(
+        multiagent=DEFAULT_MA,
+        output_folder=DEFAULT_OUTPUT_FOLDER,
+        gui=DEFAULT_GUI,
+        plot=False,
+        colab=DEFAULT_COLAB,
+        record_video=DEFAULT_RECORD_VIDEO,
+        local=True,
+        learning_rate=learning_rate,
+        gamma=gamma,    
+        batch_size=batch_size,
+        net_arch={'pi': pi_net_arch, 'qf': qf_net_arch},
+        ent_coef=ent_coef,
+        target_entropy=target_entropy,
+        num_envs=4 
+    )
+
+    return mean_reward
 
 if __name__ == '__main__':
     #### Define and parse (optional) arguments for the script ##
@@ -200,5 +298,19 @@ if __name__ == '__main__':
     parser.add_argument('--output_folder',      default=DEFAULT_OUTPUT_FOLDER, type=str,           help='Folder where to save logs (default: "results")', metavar='')
     parser.add_argument('--colab',              default=DEFAULT_COLAB,         type=bool,          help='Whether example is being run by a notebook (default: "False")', metavar='')
     ARGS = parser.parse_args()
+
+    # Run Optuna optimization on MqSQL database
+    #study = optuna.create_study(storage='sqlite:///my_study.db', study_name="drones", direction='maximize', load_if_exists=True)
+    study = optuna.create_study(
+        study_name="Drones_Defesa_Projeto",
+        storage='mysql+pymysql://optuna_user:Estg_12345#@localhost:3306/optuna_db',
+        direction='maximize',
+        load_if_exists=True
+    )
+
+    study.optimize(objective, n_trials=250)
+
+    print("Best hyperparameters: ", study.best_params)
+    print("Best reward: ", study.best_value)
 
     run(**vars(ARGS))
